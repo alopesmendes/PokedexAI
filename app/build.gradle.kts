676f2792ef8daa2266b6564a8d3ae56e5560be85
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2025-09-24 23:24:47 +0200

    :heavy_plus_sign: add project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -40,20 +41,36 @@ android {
 }
 
 dependencies {
+    // Ktor
+    implementation(libs.bundles.ktor)
 
+    // Kotlinx Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    // Koin
+    implementation(platform(libs.koin.bom))
+    implementation(libs.bundles.koin)
+
+    // Android
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+
+    // Coil
+    implementation(libs.coil.compose)
+
+    // Compose Bundle
+    implementation(platform(libs.androidx.compose.bom)) // Manages versions for Compose libraries
+    implementation(libs.bundles.androidx.compose)
+
+    // Navigation Bundle
+    implementation(libs.bundles.androidx.navigation)
+
+    // Test dependencies
+    testImplementation(libs.mockk)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform(libs.androidx.compose.bom)) // Also for test Compose library versions
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
