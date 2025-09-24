37e028c7978577d726c1a8b68dd4ae7f7dd39694
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2025-09-24 23:58:39 +0200

    Change `kotlinOptions.jvmTarget` to `kotlin.compilerOptions.jvmTarget` and add `PropertyParamAnnotationDefaultTargetMode` to `kotlin.compilerOptions.freeCompilerArgs`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        compilerOptions.freeCompilerArgs.add("-XXLanguage:+PropertyParamAnnotationDefaultTargetMode")
     }
     buildFeatures {
         compose = true
