c36e09d7c00ebbe41d03e28bfd45da8a95bb10d3
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2025-10-05 13:59:36 +0200

    :fire: remove .gitkeep

--- app/src/test/java/com/ailtontech/pokedewai/data/datasources/remote/ApiServicesTest.kt
+++ app/src/test/java/com/ailtontech/pokedewai/data/datasources/remote/ApiServicesTest.kt
@@ -5,17 +5,17 @@ import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestDispatcher // Added
-import kotlinx.coroutines.test.UnconfinedTestDispatcher // Added
-import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
-import org.junit.Test
-import java.io.IOException
-import kotlinx.serialization.SerializationException
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.io.IOException
+import kotlin.test.assertFailsWith
+import kotlinx.serialization.SerializationException
 
 @ExperimentalCoroutinesApi
 class ApiServicesTest {
@@ -34,19 +34,14 @@ class ApiServicesTest {
     @Test
     fun `execute given apiCall throws IOException should throw NetworkError`() = runTest(unconfinedTestDispatcher) {
         val originalException = IOException("Network connection lost")
-        var caughtError: Throwable? = null
 
-        try {
+        val exception = assertFailsWith<Error.NetworkError> {
             ApiServices.execute<Unit>(unconfinedTestDispatcher) {
                 throw originalException
             }
-        } catch (e: Throwable) {
-            caughtError = e
         }
-
-        assertTrue("Error.NetworkError was expected", caughtError is Error.NetworkError)
-        assertEquals("Network error: Network connection lost", caughtError?.message)
-        assertEquals(originalException, caughtError?.cause)
+        assertEquals("Network error: Network connection lost", exception.message)
+        assertEquals(originalException, exception.cause)
     }
 
     @Test
@@ -59,20 +54,15 @@ class ApiServicesTest {
         every { originalException.cause } returns null
         every { originalException.message } returns "Mocked Ktor Client Error"
 
-        var caughtError: Throwable? = null
-        try {
+        val exception = assertFailsWith<Error.HttpError> {
             ApiServices.execute<Unit>(unconfinedTestDispatcher) {
                 throw originalException
             }
-        } catch (e: Throwable) {
-            caughtError = e
         }
 
-        assertTrue("Error.HttpError was expected", caughtError is Error.HttpError)
-        val httpError = caughtError as Error.HttpError
-        assertEquals(HttpStatusCode.BadRequest.value, httpError.statusCode)
-        assertEquals("Client error: ${HttpStatusCode.BadRequest.description}", httpError.message)
-        assertEquals(originalException, httpError.cause)
+        assertEquals(HttpStatusCode.BadRequest.value, exception.statusCode)
+        assertEquals("Client request failed: Bad Request", exception.message)
+        assertEquals(originalException, exception.cause)
     }
 
     @Test
@@ -85,70 +75,51 @@ class ApiServicesTest {
         every { originalException.cause } returns null
         every { originalException.message } returns "Mocked Ktor Server Error"
 
-        var caughtError: Throwable? = null
-        try {
+        val exception = assertFailsWith<Error.HttpError> {
             ApiServices.execute<Unit>(unconfinedTestDispatcher) {
                 throw originalException
             }
-        } catch (e: Throwable) {
-            caughtError = e
         }
 
-        assertTrue("Error.HttpError was expected", caughtError is Error.HttpError)
-        val httpError = caughtError as Error.HttpError
-        assertEquals(HttpStatusCode.InternalServerError.value, httpError.statusCode)
-        assertEquals("Server error: ${HttpStatusCode.InternalServerError.description}", httpError.message)
-        assertEquals(originalException, httpError.cause)
+        assertEquals(HttpStatusCode.InternalServerError.value, exception.statusCode)
+        assertEquals("Server response error: Internal Server Error", exception.message)
+        assertEquals(originalException, exception.cause)
     }
 
     @Test
     fun `execute given apiCall throws SerializationException should throw SerializationError`() = runTest(unconfinedTestDispatcher) {
         val originalException = SerializationException("Failed to parse JSON")
-        var caughtError: Throwable? = null
 
-        try {
+        val exception = assertFailsWith<Error.SerializationError> {
             ApiServices.execute<Unit>(unconfinedTestDispatcher) {
                 throw originalException
             }
-        } catch (e: Throwable) {
-            caughtError = e
         }
-
-        assertTrue("Error.SerializationError was expected", caughtError is Error.SerializationError)
-        assertEquals("Serialization error: Failed to parse JSON", caughtError?.message)
-        assertEquals(originalException, caughtError?.cause)
+        assertEquals("Serialization error: Failed to parse JSON", exception.message)
+        assertEquals(originalException, exception.cause)
     }
 
     @Test
     fun `execute given apiCall throws custom HttpError should re-throw it`() = runTest(unconfinedTestDispatcher) {
         val customError = Error.HttpError(404, "Custom Resource Not Found", null)
-        var caughtError: Throwable? = null
 
-        try {
+        val exception = assertFailsWith<Error.HttpError> {
             ApiServices.execute<Unit>(unconfinedTestDispatcher) {
                 throw customError
             }
-        } catch (e: Throwable) {
-            caughtError = e
         }
-        assertEquals(customError, caughtError)
+        assertEquals(customError, exception)
     }
 
     @Test
     fun `execute given apiCall throws other Throwable should throw UnknownApiError`() = runTest(unconfinedTestDispatcher) {
         val originalException = IllegalArgumentException("Some other weird issue")
-        var caughtError: Throwable? = null
-
-        try {
+        val exception = assertFailsWith<Error.UnknownApiError> {
             ApiServices.execute<Unit>(unconfinedTestDispatcher) {
                 throw originalException
             }
-        } catch (e: Throwable) {
-            caughtError = e
         }
-
-        assertTrue("Error.UnknownApiError was expected", caughtError is Error.UnknownApiError)
-        assertEquals("An unknown API error occurred: Some other weird issue", caughtError?.message)
-        assertEquals(originalException, caughtError?.cause)
+        assertEquals("An unknown API error occurred: Some other weird issue", exception.message)
+        assertEquals(originalException, exception.cause)
     }
 }
